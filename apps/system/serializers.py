4b8e224858ca657e01e545b016c3c7eaa7e00541
caoqianming <caoqianming@foxmail.com>
2025-06-09 14:04:04 +0800

    feat: user_exist优化

--- apps/system/serializers.py
+++ apps/system/serializers.py
@@ -322,7 +322,7 @@ def phone_exist(phone):
 
 
 def user_exist(username):
-    if User.objects.filter(username=username).exists():
+    if User.objects.get_queryset(all=True).filter(username=username).exists():
         raise serializers.ValidationError(**USERNAME_EXIST)
     return username
 
