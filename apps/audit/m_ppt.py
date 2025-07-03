71f2a14a6bd026674e91d65ebd3f9c05742c8ca2
caoqianming <caoqianming@foxmail.com>
2025-07-03 16:50:48 +0800

    feat: 导出ppt功能2

--- apps/audit/m_ppt.py
+++ apps/audit/m_ppt.py
@@ -36,34 +36,36 @@ def export_pptx(atask:Atask, FileName:str):
             elif shape.text == "leader":
                 replace_text_preserve_formatting(shape, "leader", atask.leader.name)
 
-    slide = prs.slides.add_slide(prs.slide_layouts[2])
-    
-    # 3. 将新幻灯片移动到指定位置
-    target_position = 5
-    slides = prs.slides._sldIdLst  # 获取幻灯片列表
-    slides.insert(target_position, slides[-1])  # 将最后一张插入到目标位置
-    del slides[-1]  # 删除原位置的重复引用
+    issues = AtaskIssue.objects.filter(atask=atask).order_by("atask", "standarditem__number_sort", "-create_time")
+    for ind, issue in enumerate(issues):
+        slide = prs.slides.add_slide(prs.slide_layouts[2])
+        
+        # 3. 将新幻灯片移动到指定位置
+        # target_position = ind + 5
+        # slides = prs.slides._sldIdLst  # 获取幻灯片列表
+        # slides.insert(target_position, slides[-1])  # 将最后一张插入到目标位置
+        # del slides[-1]  # 删除原位置的重复引用
 
-    shapes = slide.shapes
+        shapes = slide.shapes
 
-    rows = cols = 2
-    left = top = Inches(2.0)
-    width = Inches(10.0)
-    height = Inches(0.8)
+        rows = cols = 2
+        left = top = Inches(2.0)
+        width = Inches(10.0)
+        height = Inches(0.8)
 
-    table = shapes.add_table(rows, cols, left, top, width, height).table
+        table = shapes.add_table(rows, cols, left, top, width, height).table
 
-    # set column widths
-    table.columns[0].width = Inches(2.0)
-    table.columns[1].width = Inches(8.0)
+        # set column widths
+        table.columns[0].width = Inches(2.0)
+        table.columns[1].width = Inches(8.0)
 
-    # write column headings
-    table.cell(0, 0).text = '条款'
-    table.cell(0, 1).text = '问题描述'
+        # write column headings
+        table.cell(0, 0).text = '条款'
+        table.cell(0, 1).text = '问题描述'
 
-    # write body cells
-    table.cell(1, 0).text = '1.1.1'
-    table.cell(1, 1).text = 'xx'
+        # write body cells
+        table.cell(1, 0).text = issue.standarditem.number if issue.standarditem else ""
+        table.cell(1, 1).text = issue.content
 
     path = f"media/temp/{FileName}_{atask.id}.pptx"
     prs.save(os.path.join(BASE_DIR, path))
