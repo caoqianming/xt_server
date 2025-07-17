0debe89ec53538d05113e17c51b21c0c9c3133f6
caoqianming <caoqianming@foxmail.com>
2025-07-18 02:00:05 +0800

    feat: 导出pptbug

--- apps/audit/m_ppt.py
+++ apps/audit/m_ppt.py
@@ -85,7 +85,14 @@ def export_pptx(atask:Atask, FileName:str, user:User):
         table.cell(0, 2).text = '问题描述'
 
         # write body cells
-        table.cell(1, 0).text = f'{issue.standarditem.number} {issue.standarditem.parent.parent.content}'  if issue.standarditem else ""
+        standarditem = issue.standarditem
+        if standarditem.level == 30:
+            yj = standarditem.parent.parent.content
+        elif standarditem.level == 20:
+            yj = standarditem.parent.content
+        elif standarditem.level == 10:
+            yj = standarditem.content
+        table.cell(1, 0).text = f'{issue.standarditem.number} {yj}'  if issue.standarditem else ""
         table.cell(1, 1).text = R_LEVEL_DICT.get(issue.risk_level, "")
         table.cell(1, 2).text = issue.content
 
@@ -134,6 +141,6 @@ def export_pptx(atask:Atask, FileName:str, user:User):
                 )
                 current_left += img_widths[i] + spacing  # 移动到下一张图片位置
 
-    path = f"media/temp/{FileName}_{atask.id}_{user.name}.pptx"
-    prs.save(os.path.join(BASE_DIR, path))
+    path = f"/media/temp/{FileName}_{atask.id}_{user.name}.pptx"
+    prs.save(BASE_DIR+path)
     return path
