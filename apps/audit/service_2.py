41b21aecd64a4c0414d520dbfc4d24ac6558ebd2
caoqianming <caoqianming@foxmail.com>
2025-07-18 11:21:02 +0800

    feat: 导出word可选择type2

--- apps/audit/service_2.py
+++ apps/audit/service_2.py
@@ -46,8 +46,9 @@ def export_issue_excel(atask:Atask):
 
 
 
-templ2 = os.path.join(BASE_DIR, "media/muban/xxx任务问题清单.docx")
+
 def export_issue_docx(atask:Atask, type=1):
+    templ2 = os.path.join(BASE_DIR, "media/muban/xxx任务问题清单.docx")
     if type == 2:
         templ2 = os.path.join(BASE_DIR, "media/muban/xxx任务问题清单2.docx")
     doc = DocxTemplate(templ2)
