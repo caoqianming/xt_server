9f239d54f284508bf69b7f9e0bec534245554c78
caoqianming <caoqianming@foxmail.com>
2025-10-17 12:32:18 +0800

    feat: base 获取流转时排序按attribute_type倒序

--- apps/wf/services.py
+++ apps/wf/services.py
@@ -77,7 +77,7 @@ class WfService(object):
         """
         获取状态可执行的操作
         """
-        return Transition.objects.filter(is_deleted=False, source_state=state).all().order_by("attribute_type", "id")
+        return Transition.objects.filter(is_deleted=False, source_state=state).all().order_by("-attribute_type", "-id")
 
     @classmethod
     def get_ticket_steps(cls, ticket: Ticket):
